6eef8ecff57e62c4604b8574d2175b9b0e7db9ab
王逸洲 <8462198+wx_b22a5e0bd6@user.noreply.gitee.com>
2022-12-15 10:10:00 +0800

    2.0修改hello world

--- TestObject.java
+++ TestObject.java
@@ -4,7 +4,6 @@ public class TestObject {
     int ability_level;
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         //bug 修改
         System.out.println("hello个头");
     }
