a160e3bbb9d8337d017b1fc48497859eac01c68d
王逸洲 <8462198+wx_b22a5e0bd6@user.noreply.gitee.com>
2022-12-15 09:52:39 +0800

    第一次提交

---
+++ TestObject.java
@@ -0,0 +1,9 @@
+public class TestObject {
+    String name;
+    int age;
+    int ability_level;
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
